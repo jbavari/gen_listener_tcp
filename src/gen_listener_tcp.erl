a2fe081bc2cda0a4921fb90d19ce95a5f8f16427
Josh Bavari <jbavari@canvas.technology>
2018-05-03 11:06:09 -0600

    Add back in callbacks as a variable

--- src/gen_listener_tcp.erl
+++ src/gen_listener_tcp.erl
@@ -37,18 +37,7 @@
 	 }).
 
 
-behaviour_info(callbacks) ->
-    [
-     {init, 1},
-     {handle_accept, 2},
-     {handle_call, 3},
-     {handle_cast, 2},
-     {handle_info, 2},
-     {terminate, 2},
-     {code_change, 3}
-    ].
-
-behaviour_info(optional_callbacks) ->
+behaviour_info(Callbacks) ->
     [
      {init, 1},
      {handle_accept, 2},
